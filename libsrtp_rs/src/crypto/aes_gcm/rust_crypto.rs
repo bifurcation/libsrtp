6ece6d7a84b019082720a3abcdac1b11b0264372
Richard L. Barnes <richbarn@cisco.com>
2021-09-09 16:16:57 -0400

    Add GCM logic for OpenSSL (doesn't pass tests)

--- libsrtp_rs/src/crypto/aes_gcm/rust_crypto.rs
+++ libsrtp_rs/src/crypto/aes_gcm/rust_crypto.rs
@@ -1,5 +1,6 @@
-use super::constants::AesKeySize;
-use super::{xor_eq, Cipher, CipherType, CipherTypeID, Reset};
+#![cfg(feature = "rust-crypto")]
+use crate::crypto::constants::AesKeySize;
+use crate::crypto::{xor_eq, Cipher, CipherType, CipherTypeID, Reset};
 use crate::replay::ExtendedSequenceNumber;
 use crate::srtp::Error;
 use aes_gcm::aead::{generic_array::GenericArray, AeadInPlace, NewAead};
@@ -216,83 +217,5 @@ impl CipherType for AesGcm {
         Box::new(AesGcm {
             key_size: self.key_size,
         })
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use crate::crypto::self_test;
-    use hex_literal::hex;
-
-    #[test]
-    fn test_128() -> Result<(), Error> {
-        let cipher_type = AesGcm::new(AesKeySize::Aes128)?;
-        assert_eq!(cipher_type.id(), CipherTypeID::AesGcm128);
-
-        let tests_passed = self_test::cipher(&cipher_type)?;
-        assert!(tests_passed > 0);
-
-        Ok(())
-    }
-
-    #[test]
-    fn test_256() -> Result<(), Error> {
-        let cipher_type = AesGcm::new(AesKeySize::Aes256)?;
-        assert_eq!(cipher_type.id(), CipherTypeID::AesGcm256);
-
-        let tests_passed = self_test::cipher(&cipher_type)?;
-        assert!(tests_passed > 0);
-
-        Ok(())
-    }
-
-    #[test]
-    fn test_rtp_example() -> Result<(), Error> {
-        let key = hex!("238c882f36f000301573e69383502d9d");
-        let salt = hex!("f2fee04070fc3f65d706e2e4");
-        let ssrc: u32 = 0xcafebabe;
-        let ext_seq_num: ExtendedSequenceNumber = 0x0000001234;
-        // f2fee04070fc3f65d706e2e4 ^ 0000cafebabe000000001234
-        let expected_nonce: [u8; 12] = hex!("f2fe2abeca423f65d706f0d0");
-        let aad = hex!(
-            "900f1234decafbadcafebabebede00061712e0205bfa949b1c220000c830bb46732778d9929aab00"
-        );
-        let pt = [0xab; 16];
-        let ct = hex!("0eca0cf95ee955b26cd3d288b49f6ca9f4b1b759719eb5bc113b9ff1d40cd25a");
-
-        let cipher_type = AesGcm::new(AesKeySize::Aes128)?;
-        let mut cipher = cipher_type.create(&key, &salt)?;
-
-        // Verify correct nonce formation
-        let mut nonce: [u8; 12] = Default::default();
-        cipher.rtp_nonce(ssrc, ext_seq_num, &mut nonce)?;
-        assert_eq!(nonce, expected_nonce);
-
-        // Verify correct encryption
-        let mut enc_buffer = [0u8; 32];
-        enc_buffer[..pt.len()].copy_from_slice(&pt);
-
-        cipher.reset();
-        cipher.add_aad(&aad)?;
-        cipher.set_nonce(&nonce)?;
-        let ct_size = cipher.encrypt(&mut enc_buffer, pt.len())?;
-        assert_eq!(ct_size, ct.len());
-        assert_eq!(enc_buffer, ct);
-
-        // Verify correct decryption
-        cipher.reset();
-        cipher.add_aad(&aad)?;
-        cipher.set_nonce(&nonce)?;
-        let pt_size = cipher.decrypt(&mut enc_buffer)?;
-        assert_eq!(pt_size, pt.len());
-        assert_eq!(&enc_buffer[..pt_size], &pt);
-
-        Ok(())
-    }
-
-    #[test]
-    fn test_rtcp_example() {
-        // TODO
     }
 }
